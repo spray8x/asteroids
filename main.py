a4f5a17df45334db4a104b13e651605df24294cb
spray8x <vjpd2107@proton.me>
2025-09-22 11:37:32 +0000

    C: Added Screen

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    running = True
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
     print("Starting Asteroids!")
-
+    print("Screen width: 1280")
+    print("Screen height: 720")
 
 if __name__ == "__main__":
     main()
