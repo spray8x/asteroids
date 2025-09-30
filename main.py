b5c3c7e22e77feda3678c41290e843f0face7daa
spray8x <vjpd2107@proton.me>
2025-09-30 06:34:36 +0000

    F: Added Player and movement

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    running = True
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick()/1000
 
-    print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
